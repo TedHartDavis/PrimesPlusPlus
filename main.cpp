cc73d70f509000366a66ebf7f723d1fa2d98de1a
Tedster <tedster@tedster.net>
2018-04-04 05:30:27 +0100

    attempt 1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,68 @@
 #include <iostream>
 
-using namespace std;
+// Stolen from BasiCalc
+int getnumber ()
+{
+    //std::getline(std::cin, number);
+    std::string placeholder;
+    getline(std::cin, placeholder);
+    //number << std::atof << placeholder << std::endl;
+    int number = atoi(placeholder.c_str());
+    return number;
+}
+// End stolen
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int lower_bound, upper_bound;
+    lower_bound = getnumber();
+    upper_bound = getnumber();
+    std::cout << "fff" << std::endl;
+    std::cout << lower_bound << std::endl;
+    std::cout << upper_bound << std::endl;
+
+    if (lower_bound < 1)
+    {
+        std::cout << "Lower bound too low" << std::endl;
+        return 1;
+    }
+    else if (upper_bound <= lower_bound)
+    {
+        std::cout << "Upper bound too low" << std::endl;
+    }
+    else
+    {
+        std::cout << "Good" << std::endl;
+    }
+
+    int i;
+    int j;
+    bool NotPrime;
+    for(i = lower_bound+1; i<=upper_bound; i++)
+    {
+        NotPrime = true;
+        if (i%2==0)
+        {
+            continue;
+        }
+
+        //std::cout << i << std::endl;
+
+        for(j = i; j>lower_bound+1; j--)
+        {
+            if (NotPrime == false)
+            {
+                break;
+            }
+            if (j%2!=0)
+            {
+                NotPrime = true;
+                continue;
+            }
+            //std::cout << j << std::endl;
+            if (i%j>0)
+                std::cout << i << " may be prime " << std::endl;
+        }
+    }
 }
+
